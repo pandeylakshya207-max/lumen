bccfdd117690375a49a729f831023a2ffb3ffb71
Lakshya Pandey <pandeylakshya207@gmail.com>
2026-08-22 19:29:27 +0000

    feat(lexer): add advance() with newline-aware line/col tracking; add skip_whitespace() with // comment support

--- src/lexer.rs
+++ src/lexer.rs
@@ -15,4 +15,22 @@ impl<'a> Lexer<'a> {
     pub fn is_at_end(&self) -> bool { self.pos >= self.src.len() }
     pub fn peek(&self) -> Option<u8> { self.src.get(self.pos).copied() }
     pub fn peek_next(&self) -> Option<u8> { self.src.get(self.pos + 1).copied() }
+
+    pub fn advance(&mut self) -> u8 {
+        let b = self.src[self.pos];
+        self.pos += 1;
+        if b == b'\n' { self.line += 1; self.col = 1; } else { self.col += 1; }
+        b
+    }
+
+    pub fn skip_whitespace(&mut self) {
+        loop {
+            while let Some(b) = self.peek() {
+                if b.is_ascii_whitespace() { self.advance(); } else { break; }
+            }
+            if self.peek() == Some(b'/') && self.peek_next() == Some(b'/') {
+                while let Some(b) = self.peek() { self.advance(); if b == b'\n' { break; } }
+            } else { break; }
+        }
+    }
 }
